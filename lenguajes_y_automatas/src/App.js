b90cc129915d1af6e60ff4cfc1b960be54ff2a6b
Diego Armando Payán López <DiegoGMR@users.noreply.github.com>
2018-09-29 00:31:04 -0600

    Diseño base Fase 1:Introduccion datos

--- lenguajes_y_automatas/src/App.js
+++ lenguajes_y_automatas/src/App.js
@@ -1,18 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./estilos.css";
+import Ionicon from 'react-ionicons';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="row">
+
+          <div className="col-md-2 col-2">
+          </div>
+          <div className="col-md-10 col-10 header row">
+            <div className="col-md-1 col-1 navbar-item">
+              <Ionicon icon="logo-javascript" color="#ffff62" fontSize="14"/> App.js
+            </div>
+            <div className="col-md-1 col-1 navbar-item">
+             <Ionicon icon="logo-css3" color="#d4d4ef" fontSize="14"/> estilos.css
+            </div>
+            <div className="col-md-1 col-1 navbar-item-selected">
+             <Ionicon icon="logo-javascript" color="#ffff62" fontSize="14"/> Compilador.js
+            </div>
+            <div className="col-md-1 col-1 navbar-execute">
+              <a href="" className=""><Ionicon icon="ios-play" color="white" fontSize="14"/> EJECUTAR</a>
+           </div>
+          </div>
+
+          <div className="col-md-2 col-2 left-bar row">
+            <div className="col-md-2 col-2 config-bar">
+              <div className="col-md-12 col-12">
+                <Ionicon icon="md-document" color="white" fontSize="40" className="config-bar-icon"/>
+                <Ionicon icon="md-search" color="#e0e0e0" fontSize="40" className="config-bar-icon"/>
+                <Ionicon icon="ios-git-network" color="#e0e0e0" fontSize="40" className="config-bar-icon"/>
+                <Ionicon icon="ios-bug" color="#e0e0e0" fontSize="40" className="config-bar-icon"/>
+                <Ionicon icon="ios-exit" color="#e0e0e0" fontSize="40" className="config-bar-icon"/>
+                <Ionicon icon="md-cog" color="#e0e0e0" fontSize="35" className="config-bar-config"/>
+              </div>
+            </div>
+            <div className="col-md-10 col-10">
+              <div className="col-md-12 col-12 element ml-4 mt-3">EXPLORER</div>
+              <div className="col-md-12 col-12 menu-drops ml-2 mt-1"><Ionicon icon="md-arrow-dropright" color="white"/> Open Editors <span class="badge badge-primary"> 1 Unsaved</span></div>
+              <div className="col-md-12 col-12 menu-drops ml-2"><Ionicon icon="md-arrow-dropright" color="white" className="arrow-open"/> Lenguajes_y_automatas</div>
+              <div className="col-md-12 col-12 element ml-2 mt-1"><Ionicon icon="md-arrow-dropright" color="white"/><Ionicon icon="md-folder" color="#6262ff"/> .vs</div>
+              <div className="col-md-12 col-12 element ml-2"><Ionicon icon="md-arrow-dropright" color="white" className="arrow-open"/><Ionicon icon="md-folder-open" color="#6262ff"/> icon</div>
+              <div className="col-md-12 col-12 element ml-5"><Ionicon icon="md-image" color="#6262ff"/> vs-icon.png</div>
+              <div className="col-md-12 col-12 element ml-2"><Ionicon icon="md-arrow-dropright" color="white"/><Ionicon icon="md-folder" color="#6262ff"/> js</div>
+              <div className="col-md-12 col-12 element ml-2"><Ionicon icon="md-arrow-dropright" color="white" className="arrow-open"/><Ionicon icon="md-folder-open" color="#6262ff"/> lenguajes_y_automatas</div>
+              <div className="col-md-12 col-12 element ml-5"><Ionicon icon="md-arrow-dropright" color="white"/><Ionicon icon="md-folder" color="#00b100"/> node_modules</div>
+              <div className="col-md-12 col-12 element ml-5"><Ionicon icon="md-arrow-dropright" color="white" className="arrow-open"/><Ionicon icon="md-folder" color="#6262ff"/> public</div>
+              <div className="col-md-12 col-12 element ml-5"><Ionicon icon="md-arrow-dropright" color="white" className="arrow-open"/><Ionicon icon="md-folder-open" color="#6262ff"/> src</div>
+              <div className="col-md-12 col-12 element-in ml-5"><Ionicon icon="logo-javascript" color="#ffff62"/> acciones.js</div>
+              <div className="col-md-12 col-12 element-in ml-5"><Ionicon icon="logo-css3" color="#d4d4ef"/> App.css</div>
+              <div className="col-md-12 col-12 element-in ml-5"><Ionicon icon="logo-javascript" color="#ffff62"/> App.js</div>
+              <div className="col-md-12 col-12 element-in ml-5"><Ionicon icon="logo-javascript" color="#ffff62"/> Compilador.js</div>
+              <div className="col-md-12 col-12 element-in ml-5"><Ionicon icon="logo-css3" color="#d4d4ef"/> estilos.css</div>
+              <div className="col-md-12 col-12 element-in ml-5"><Ionicon icon="logo-css3" color="#d4d4ef"/> index.css</div>
+              <div className="col-md-12 col-12 element-in ml-5"><Ionicon icon="logo-javascript" color="#ffff62"/> index.js</div>
+              <div className="col-md-12 col-12 element ml-5"><Ionicon icon="md-git-branch" color="#d90000"/> .gitignore</div>
+              <div className="col-md-12 col-12 element ml-5"><Ionicon icon="logo-html5" color="#d90000"/> index.html</div>
+            </div>
+          </div>
+
+          <div className="col-md-2 col-2">
+          </div>
+          <div className="col-md-10 col-10 code">
+            <textarea ref="codigo" spellCheck="false" rows="36"></textarea>
+          </div>
+
+          <div className="col-md-2 col-2">
+          </div>
+          <div className="col-md-10 col-10 output">
+            Aqui van las diferentes salidas, que flojera hacerlo orita :)
+          </div>
+
       </div>
     );
   }
